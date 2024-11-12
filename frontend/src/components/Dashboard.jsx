58eb695b6ea16e4891050751284685d1e0d6fe29
JhonLesterY <2201103184@student.buksu.edu.ph>
2024-11-12 16:57:29 +0800

    dashboard/sidebar

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,71 +1,49 @@
 import { NavLink } from "react-router-dom";
 import User from "/src/images/user.png";
+
 const Dashboard = () => {
   return (
-    <>
-      {/* Navbar */}
-      <div className="h-full fixed text-center bg-customblue text-slate-200 w-[15rem] justify-items-center ">
+    <div className="h-full fixed text-center bg-customblue text-slate-200 w-[15rem] flex flex-col justify-between">
+      {/* Navbar Header */}
+      <div>
         <NavLink to="/dashboard">
           <div className="text-2xl pt-5 font-semibold mb-3">Unicare</div>
-
-          <div className="border w-44 mb-9"></div>
+          <div className="border w-44 mx-auto mb-9"></div>
         </NavLink>
 
-        <div className="font-medium text-lg mb-5">
-          <NavLink to="/Profile" className="flex gap-1">
-            <div>
-              <img className="h-8 " src={User} alt="img" />
-            </div>
-            <div className="my-auto">Profile</div>
-          </NavLink>
-        </div>
-        <ul>
-          <li className="cursor-pointer mb-2">
-            <NavLink to="/dashboard" className=" font-medium text-lg">
+        {/* Navigation Links */}
+        <ul className="space-y-4 font-medium text-lg">
+          <li>
+            <NavLink to="/dashboard" activeClassName="text-white" className="hover:text-gray-200">
               Dashboard
             </NavLink>
           </li>
-          <li className="mb-2  font-medium text-lg">
-            <NavLink to="/Appointment">Appointment</NavLink>
+          <li>
+            <NavLink to="/appointment" activeClassName="text-white" className="hover:text-gray-200">
+              Appointment
+            </NavLink>
           </li>
-
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-
-          <li className=" font-medium text-lg mb-3">
-            <NavLink to="/feedback">Feedback</NavLink>
-          </li>
-
-          <li className=" font-medium text-lg mb-3">
-            <NavLink to="/about-us">About us</NavLink>
-          </li>
-
-          <li className=" font-medium text-lg mb-3">
-            <NavLink to="/settings">Settings</NavLink>
+          <li>
+            <NavLink to="/services" activeClassName="text-white" className="hover:text-gray-200">
+              Services
+            </NavLink>
           </li>
         </ul>
       </div>
-    </>
+
+      {/* Bottom Links */}
+     <div className="flex flex-col items-center space-y-3 pb-6 border-t border-gray-600 pt-4">
+        <NavLink to="/feedback" activeClassName="text-white" className="hover:text-gray-200">
+          Feedback
+        </NavLink>
+        <NavLink to="/about-us" activeClassName="text-white" className="hover:text-gray-200">
+          About Us
+        </NavLink>
+        <NavLink to="/settings" activeClassName="text-white" className="hover:text-gray-200">
+          Settings
+        </NavLink>
+      </div>
+    </div>
   );
 };
 
